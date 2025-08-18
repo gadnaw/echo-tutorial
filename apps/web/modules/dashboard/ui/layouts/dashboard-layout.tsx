f768d6b894c3099c7a72f92d0cee82ca4ae3ad63
phuocthai-athena <thaizzdn1999@gmail.com>
2025-08-18 12:15:05 +0700

    18: dashboard chat

--- apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
+++ apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
@@ -1,3 +1,4 @@
+import { Provider } from "jotai";
 import { cookies } from "next/headers";
 
 import { AuthGuard } from "@/modules/auth/ui/components/auth-guard";
@@ -16,10 +17,12 @@ export const DashboardLayout = async ({
   return (
     <AuthGuard>
       <OrganizationGuard>
-        <SidebarProvider defaultOpen={defaultOpen}>
-          <DashboardSidebar />
-          <main className="flex flex-1 flex-col">{children}</main>
-        </SidebarProvider>
+        <Provider>
+          <SidebarProvider defaultOpen={defaultOpen}>
+            <DashboardSidebar />
+            <main className="flex flex-1 flex-col">{children}</main>
+          </SidebarProvider>
+        </Provider>
       </OrganizationGuard>
     </AuthGuard>
   );
